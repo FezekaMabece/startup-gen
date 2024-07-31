2a392d53b1597bad70e8dfbe7232bc09c40c34ed
fmabece023 <fmabece023@student.wethinkcode.co.za>
2024-07-31 15:11:27 +0200

    back end connected

--- src/TrendingProducts.js
+++ src/TrendingProducts.js
@@ -1,33 +1,36 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import moment from 'moment';
 
 const currentDate = new Date().toDateString();
 const TrendingProducts = () => {
   const [products, setProducts] = useState([]);
+  const getProducts = async () => {
+    await axios.get(' https://nerd-biz-bot.vercel.app/products')
+    .then((res) => {
+      console.log(res);
+      setProducts(res.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
 
   useEffect(() => {
-    
-    const mockProducts = [
-      { name: 'product1', image_url: 'image1', price: '$10', scrapped_date: currentDate, url: 'Click Here To View Product' },
-      { name: 'product2', image_url: 'image2', price: '$20', scrapped_date: currentDate, url: 'Click Here To View Product' },
-      { name: 'product3', image_url: 'image3', price: '$30', scrapped_date: currentDate, url: 'Click Here To View Product' },
-      { name: 'product4', image_url: 'image4', price: '$40', scrapped_date: currentDate, url: 'Click Here To View Product' },
-      { name: 'product5', image_url: 'image5', price: '$50', scrapped_date: currentDate, url: 'Click Here To View Product' },
-    ];
-
-    setProducts(mockProducts);
-  }, []);
+    getProducts()
+  }, [])
 
   return (
     <div className="trending-products">
       <h2>Trending Products</h2>
       <div className="products-grid">
         {products.map(product => (
-          <div key={product.name} className="product-card">
+          <div key={product.product_name} className="product-card">
             <img src={product.image_url} alt={product.name} />
-            <h3>{product.name}</h3>
+            <p>{product.product_name}</p>
             <p>Price: {product.price}</p>
-            <p>Date: {product.scrapped_date}</p>
-            <a href={product.url} target="_blank" rel="noopener noreferrer">Click Here To View Product</a>
+            <p>Date Sourced: {moment(product.date_scraped).utc().format('YYYY-MM-DD')}</p>
+            <a href={product.source} target="_blank" rel="noopener noreferrer">Click Here To View Product</a>
           </div>
         ))}
       </div>
