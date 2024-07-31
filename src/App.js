16d7323d0e121fa1b7ce662d9e439dd21da72c43
Lebang Nong <lebangnong@gmail.com>
2024-08-01 01:32:29 +0200

    changed to StartupGen

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Startup-gen</h1>
+        <h1>StartupGen</h1>
       </header>
       <main>
         <TrendingProducts />
